e46cb67b4b35c115055935ba7924e777eb9637ff
andreev-oe <andreev.oe@yandex.ru>
2023-04-12 21:49:56 +0300

    feat: add task 2-3-11

--- module-2/2-3/2-3-11.js
+++ module-2/2-3/2-3-11.js
@@ -1,40 +1,24 @@
 class Calc {
-    constructor(startValue) {
-        this.startValue = startValue;
-        this.resultValue = 0
+    constructor(startValue = 0) {
+        this.resultValue = startValue
     }
     add(number) {
-        if (this.resultValue === 0) {
-            this.resultValue = this.startValue + number
-            return this
-        }
-        this.resultValue += number
-
-        return this
+        return new Calc(this.resultValue + number)
     }
     sub(number) {
-        if (this.resultValue === 0) {
-            this.resultValue = this.startValue - number
-            return this
-        }
-        this.resultValue -= number
-        return this
+        return new Calc(this.resultValue - number)
     }
     result() {
         return this.resultValue
     }
 }
 
-const calc = new Calc();
-calc.result(); // 0
-calc.add(5).result(); // 0 + 5 = 5
-calc.add(3).sub(10).result(); // 0 + 3 - 10 = -7
-
-const ten = calc.add(10);
-ten.sub(5).result(); // 10 - 5 = 5
-ten.result(); // 10
-
-const calc1 = new Calc(10);
-console.log(calc1.add(5).sub(2).sub(25).add(1).result())
-// console.log(calc1.add(5).add(50).sub(55).result())
+// const calc = new Calc();
+// calc.result(); // 0
+// calc.add(5).result(); // 0 + 5 = 5
+// calc.add(3).sub(10).result(); // 0 + 3 - 10 = -7
+//
+// const ten = calc.add(10);
+// ten.sub(5).result(); // 10 - 5 = 5
+// ten.result(); // 10
 
