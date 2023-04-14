259876fca956d3ad0499a127b5be57d6afa94a91
andreev-oe <andreev.oe@yandex.ru>
2023-04-14 17:25:20 +0300

    WIP: add task 3-2-2

--- module-3/3-2/3-2-2.js
+++ module-3/3-2/3-2-2.js
@@ -1,10 +1,25 @@
 function arrayToCsv(data) {
-
+    let resultString = ''
+    const regExp1 = /["]/gm
+    data.forEach((element) => {
+        element.forEach((item,index) => {
+            if (typeof item !== 'string' && typeof item !== 'number') {
+                throw new Error('Unexpected value')
+            }
+            if (typeof item === 'string') {
+                element[index] = item.replace(regExp1, '\"\"')
+                element[index] = `\"${element[index]}\"`
+            }
+        })
+        resultString = resultString + `${element.toString()}\n`
+    })
+    return resultString
 }
 
+arrayToCsv([['"text"', 'other "long" text']])
 arrayToCsv([[1, 2], ['a', 'b']])
-// '1,2
-//  a,b'
+// 1,2
+// a,b
 arrayToCsv([[1, 2], ['a,b', 'c,d']])
-// '1,2
-// "a,b","c,d
+// 1,2
+// "a,b","c,d"
