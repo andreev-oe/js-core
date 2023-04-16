11bcf244dda462f3311409fd23a3c6f1b22154ef
andreev-oe <andreev.oe@yandex.ru>
2023-04-16 21:48:12 +0300

    feat: add task 3-3-4

---
+++ module-3/3-3/3-3-4.js
@@ -0,0 +1,42 @@
+const { getUserInfo, getUsersIds } = db;
+
+function getUsersInfo(onLoad) {
+    getUsersIds((ids) => {
+        if (ids.length === 0) {
+            return onLoad([])
+        }
+        const usersInfo = new Array(ids.length)
+        let count = 0;
+        ids.forEach((id, index) => {
+            getUserInfo(id, info => {
+                usersInfo[index] = info
+                count++
+                if (count === usersInfo.length) {
+                    onLoad(usersInfo)
+                }
+            })
+        })
+    })
+}
+
+// const users = [
+//     {"lastName": "Ivanov", "name": "Pavel", "profession": "programmer"},
+//     {"name": "Petr"},
+//     {"city": "St. Petersburg", "name": "Lena"},
+//     {"age": 25, "hobbys": ["traveling", "macrame"], "name": "Olesya"},
+//     {"name": "Egor"}
+// ]
+//
+// getUsersIds((ids) => {
+//     console.log(ids); // ['id2', 'id6']
+// });
+//
+// getUserInfo('someUserId', (userInfo) => {
+//     console.log(userInfo); // { name: 'Alex', age: 70 }
+// });
+//
+// getUsersInfo((users) => {
+//     console.log(users); // [ { name: 'Alex', age: 70 }, { name: 'Elon' } ]
+// });
+
+
