f7a58d3733c393cb8bcfbdd0ebfec0c1185ade01
andreev-oe <andreev.oe@yandex.ru>
2023-04-24 17:36:22 +0300

    feat: append cards in DOM on repo name click
    - add basic styles

--- module-4/4-3/4-3-7.js
+++ module-4/4-3/4-3-7.js
@@ -1,6 +1,8 @@
 const DEBOUNCE_TIME = 500
 const DROP_DOWN_REPOS_COUNT = 5
-const searchField = document.querySelector('.search-filed')
+const searchFieldElement = document.querySelector('.search-field')
+const cardsContainerElement = document.querySelector('.cards-container')
+const dropDownMenuElement = document.querySelector('.drop-down-menu')
 
 const debounce = (fn, debounceTime) => {
     let debounceTimer
@@ -13,34 +15,83 @@ const debounce = (fn, debounceTime) => {
         }, debounceTime)
     }
 };
+
 const getRepos = async (repoName) => {
     const response = await fetch(`https://api.github.com/search/repositories?q=${repoName}`)
     const data = await response.json()
     const firstRepos = []
     for (let i = 0; i < DROP_DOWN_REPOS_COUNT; i++) {
+        if (!data.items[i]) {
+           break
+        }
         firstRepos.push(data.items[i])
     }
     return firstRepos
 }
 
-const showReposList = (reposList) => {
-    reposList.forEach((repo) => {
-        console.log(repo.name)
-        console.log(repo.owner.login)
-        console.log(repo.stargazers_count)
-        console.log('====================')
-    })
+const createRepoCard = (evt) => {
+    const datasetName = iop.filter((repo) => repo.name === evt.target.dataset.repoName)
+    console.log(datasetName)
+    if (datasetName) {
+        let {name, owner: {login}, stargazersCount} = datasetName[0]
+        const repoCard = document.createElement('div');
+        const repoName = document.createElement('p');
+        const repoOwner = document.createElement('p');
+        const repoStars = document.createElement('p');
+
+        repoName.textContent = `Name: ${name}`
+        repoOwner.textContent = `Name: ${login}`
+        repoStars.textContent = `Name: ${stargazersCount}`
+
+        repoCard.append(repoName, repoOwner, repoStars)
+        cardsContainerElement.append(repoCard)
+    }
 }
 
+const createDropDownMenuItem = (repo) => {
+    const repoNameElement = document.createElement('li')
+    repoNameElement.textContent = repo.name
+    repoNameElement.classList.add('repo-name')
+    repoNameElement.dataset.repoName = repo.name
+    dropDownMenuElement.append(repoNameElement)
+}
 
+const showReposList = (reposList) => {
+    dropDownMenuElement.innerHTML = ""
+    reposList.forEach((repo) => {
+        createDropDownMenuItem(repo)
+})}
 
 const searchFieldHandler = (evt) => {
-    console.log(evt.target.value)
-    getRepos(evt.target.value)
-        .then((reposList) => showReposList(reposList))
-
+    if (evt.target.value.trim()) {
+        getRepos(evt.target.value)
+            .then((reposList) => showReposList(reposList))
+    } else {
+        dropDownMenuElement.innerHTML = ""
+    }
 }
 
+searchFieldElement.addEventListener('input', debounce(searchFieldHandler, DEBOUNCE_TIME))
+dropDownMenuElement.addEventListener('click', createRepoCard)
 
+const iop = [{
+    name: 'NAME1',
+    owner: {
+        login: 'LOGIN1'
+    },
+    stargazersCount: 123
+}, {
+    name: 'NAME2',
+    owner: {
+        login: 'LOGIN2'
+    },
+    stargazersCount: 123
+}, {
+    name: 'NAME3',
+    owner: {
+        login: 'LOGIN3'
+    },
+    stargazersCount: 123
+}]
 
-searchField.addEventListener('input', debounce(searchFieldHandler, DEBOUNCE_TIME))
+showReposList(iop)
